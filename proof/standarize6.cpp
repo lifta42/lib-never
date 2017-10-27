ad0cbd9477f08da7f02a1fec0d4ff5a3ebeaa311
lifta42 <lifta42@outlook.com>
2017-10-27 18:41:35 +0800

    Almost finish the last proof of s12n

--- proof/standarize6.cpp
+++ proof/standarize6.cpp
@@ -24,6 +24,7 @@ template <typename T, typename U> struct NestProc<T, U> {
 template <typename T, typename... Us> struct NestProc<T, Us...> {
   using Type = Proc<T, typename NestProc<Us...>::Type>;
 };
+template <typename... Ts> using NProc = typename NestProc<Ts...>::Type;
 
 template <typename T> struct RevGenProc : public RevGenProc<decltype(&T::operator())> {};
 template <typename T, typename... Us, typename C>
@@ -33,10 +34,15 @@ struct RevGenProc<void (C::*)(Epct<T>, Epct<PartGen<Us...>>) const> {
   using ProcType = typename NestProc<Us..., T>::Type;
 };
 
-template <typename T> struct RevProc;
-template <typename T, typename U> struct RevProc<Proc<T, U>> {
+template <typename T> struct RevProc : public RevProc<decltype(&T::operator())> {};
+template <typename T, typename U, typename C> struct RevProc<void (C::*)(T &&, Epct<U>) const> {
   using ArgType = T;
   using RetType = U;
+};
+
+template <typename T> struct RevEpct : public RevEpct<decltype(&T::operator())> {};
+template <typename T, typename C> struct RevEpct<void (C::*)(T &&) const> {
+  using Type = T;
 };
 
 template <typename... Ts> struct Args;
@@ -102,14 +108,62 @@ constexpr auto stand(GP &&gp) {
   }
 }
 
+template <typename... Ts> struct Last;
+template <typename T> struct Last<T> {
+  using Type = T;
+};
+template <typename T, typename... Us> struct Last<T, Us...> {
+  using Type = typename Last<Us...>::Type;
+};
+
+template <typename P1, typename P2> constexpr auto pipe(P1 &&proc1, P2 &&proc2) {
+  using T = typename RevProc<P1>::ArgType;
+  using U = typename RevProc<P1>::RetType;
+  using V = typename RevProc<P2>::RetType;
+  static_assert(is_same<typename RevProc<P2>::ArgType, U>::value,
+    "Mismatch procedure type.");
+
+  return [&] (Epct<Proc<T, V>> pass) {
+    pass([&] (T &&t, Epct<V> pass) {
+      proc1(move(t), [&] (U &&u) {
+        proc2(move(u), pass);
+      });
+    });
+  };
+}
+
+template <typename P1, typename... Ps> constexpr auto pipe(P1 &&proc1,
+  Ps &&...procs) {
+  using T = typename RevProc<P1>::ArgType;
+  using U = typename RevProc<P1>::RetType;
+  using V = typename RevProc<typename Last<Ps...>::Type>::RetType;
+  return [&] (Epct<Proc<T, V>> &&pass) {
+    pipe(move(procs)...)([&] (Proc<U, V> &&proc2) {
+      pipe(move(proc1), move(proc2))(move(pass));
+    });
+  };
+}
+
+template <typename T, typename C> constexpr auto feed(T &&fed, C pass) {
+  using U = typename RevEpct<C>::Type;
+  return [&] (Proc<T, U> &&proc) {
+    proc(move(fed), pass);
+  };
+}
+
+
 int main() {
-  stand([] (Epct<int> pass, Epct<PartGen<int, int>> intern_pass) {
-    intern_pass([&] (int &&x, int &&y) {
-      pass(x + y);
-    });
-  })(3, [] (Proc<int, int> &&proc) {
-    proc(5, [] (int &&eight) {
-      cout << eight << endl;
-    });
-  });
+  pipe([] (int &&x, Epct<NProc<int, int, int>> pass) {
+    pass(stand([&] (Epct<int> pass, Epct<PartGen<int, int>> intern_pass) {
+      intern_pass([&] (int &&y, int &&z) {
+        pass(y + z - x);
+      });
+    }));
+  }, [] (NProc<int, int, int> &&add_y_z_then_sub_x, Epct<Proc<int, int>> pass) {
+    add_y_z_then_sub_x(20, pass);
+  }, [] (Proc<int, int> &&add_20_z_then_sub_x, Epct<int> pass) {
+    add_20_z_then_sub_x(30, pass);
+  })(feed(42, [] (int &&eight) {
+    cout << eight << endl;
+  }));
 }
