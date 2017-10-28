e05e671204cc667c9e89a5902b8e05f9a9f0e7a1
lifta42 <lifta42@outlook.com>
2017-10-29 02:06:44 +0800

    Format and add note

--- proof/standarize6.cpp
+++ proof/standarize6.cpp
@@ -1,21 +1,22 @@
-#include <iostream>
-#include <utility>
+// I prefer version 7, but this is also awesome.
+// note: -std=c++1z
 #include <functional>
+#include <iostream>
 #include <string>
+#include <utility>
 
 using namespace std;
 
-
 template <typename T> using Func = function<T>;
 // Expect
-template <typename T> using Epct = Func<void (T &&)>;
+template <typename T> using Epct = Func<void(T &&)>;
 // Standard Procedure, T -> U
-template <typename T, typename U> using Proc = Func<void (T &&, Epct<U>)>;
+template <typename T, typename U> using Proc = Func<void(T &&, Epct<U>)>;
 
-template <typename... Ts> using PartGen = Func<void (Ts...)>;
+template <typename... Ts> using PartGen = Func<void(Ts...)>;
 // General Procedure, Us... -> T
-template <typename T, typename... Us> using GenProc =
-  Func<void (Epct<T>, Epct<PartGen<Us...>>)>;
+template <typename T, typename... Us>
+using GenProc = Func<void(Epct<T>, Epct<PartGen<Us...>>)>;
 
 template <typename... Ts> struct NestProc;
 template <typename T, typename U> struct NestProc<T, U> {
@@ -26,7 +27,8 @@ template <typename T, typename... Us> struct NestProc<T, Us...> {
 };
 template <typename... Ts> using NProc = typename NestProc<Ts...>::Type;
 
-template <typename T> struct RevGenProc : public RevGenProc<decltype(&T::operator())> {};
+template <typename T>
+struct RevGenProc : public RevGenProc<decltype(&T::operator())> {};
 template <typename T, typename... Us, typename C>
 struct RevGenProc<void (C::*)(Epct<T>, Epct<PartGen<Us...>>) const> {
   using RetType = T;
@@ -34,13 +36,16 @@ struct RevGenProc<void (C::*)(Epct<T>, Epct<PartGen<Us...>>) const> {
   using ProcType = typename NestProc<Us..., T>::Type;
 };
 
-template <typename T> struct RevProc : public RevProc<decltype(&T::operator())> {};
-template <typename T, typename U, typename C> struct RevProc<void (C::*)(T &&, Epct<U>) const> {
+template <typename T>
+struct RevProc : public RevProc<decltype(&T::operator())> {};
+template <typename T, typename U, typename C>
+struct RevProc<void (C::*)(T &&, Epct<U>) const> {
   using ArgType = T;
   using RetType = U;
 };
 
-template <typename T> struct RevEpct : public RevEpct<decltype(&T::operator())> {};
+template <typename T>
+struct RevEpct : public RevEpct<decltype(&T::operator())> {};
 template <typename T, typename C> struct RevEpct<void (C::*)(T &&) const> {
   using Type = T;
 };
@@ -60,9 +65,7 @@ template <typename T, typename... Us> struct Args<T, Us...> {
 };
 template <typename T, typename... Us> const bool Args<T, Us...>::atom = false;
 
-
-template <typename GP>
-constexpr auto stand(GP &&gp) {
+template <typename GP> constexpr auto stand(GP &&gp) {
   using Ts = typename RevGenProc<GP>::template ArgType<Args>;
   using U = typename RevGenProc<GP>::RetType;
 
@@ -71,15 +74,13 @@ constexpr auto stand(GP &&gp) {
     // gp :: void (Epct<U>, Epct<PartGen<V>>)
     // gp :: void (Epct<U>, Epct<Func<void (V &&)>>)
     using V = typename Ts::Head;
-    return [&] (V &&v, Epct<U> pass) {
-      gp(pass, [&] (PartGen<V> &&func) {
-        func(move(v));
-      });
+    return [&](V &&v, Epct<U> pass) {
+      gp(pass, [&](PartGen<V> &&func) { func(move(v)); });
     };
-  }
-  else {
+  } else {
     using V = typename Ts::Head;
-    using Ws = typename Ts::template Tail<Args>;  // not used, but keep for `ws` below
+    using Ws =
+        typename Ts::template Tail<Args>; // not used, but keep for `ws` below
     using X = typename Ts::template Tail<PartGen>;
     using Y = typename RevGenProc<GP>::ProcType;
     using Z = typename RevProc<Y>::RetType;
@@ -90,16 +91,14 @@ constexpr auto stand(GP &&gp) {
     // Step 3, prefix the passing of argument v and return it
 
     // step 3
-    return [&] (V &&v, Epct<Z> pass) {
+    return [&](V &&v, Epct<Z> pass) {
       // step 1
       // tail_gen_proc :: Ws... -> U
       // tail_gen_proc :: void (Epct<U>, Epct<PartGen<~Ws...>>)
       // tail_gen_proc :: void (Epct<U>, Epct<X>)
-      auto tail_gen_proc = [&] (Epct<U> pass, Epct<X> intern_pass) {
-        intern_pass([&] (auto &&...ws) {
-          gp(pass, [&] (auto &&func) {
-            func(move(v), move(ws)...);
-          });
+      auto tail_gen_proc = [&](Epct<U> pass, Epct<X> intern_pass) {
+        intern_pass([&](auto &&... ws) {
+          gp(pass, [&](auto &&func) { func(move(v), move(ws)...); });
         });
       };
       // step 2
@@ -109,36 +108,33 @@ constexpr auto stand(GP &&gp) {
 }
 
 template <typename... Ts> struct Last;
-template <typename T> struct Last<T> {
-  using Type = T;
-};
+template <typename T> struct Last<T> { using Type = T; };
 template <typename T, typename... Us> struct Last<T, Us...> {
   using Type = typename Last<Us...>::Type;
 };
 
-template <typename P1, typename P2> constexpr auto pipe(P1 &&proc1, P2 &&proc2) {
+template <typename P1, typename P2>
+constexpr auto pipe(P1 &&proc1, P2 &&proc2) {
   using T = typename RevProc<P1>::ArgType;
   using U = typename RevProc<P1>::RetType;
   using V = typename RevProc<P2>::RetType;
   static_assert(is_same<typename RevProc<P2>::ArgType, U>::value,
-    "Mismatch procedure type.");
+                "Mismatch procedure type.");
 
-  return [&] (Epct<Proc<T, V>> pass) {
-    pass([&] (T &&t, Epct<V> pass) {
-      proc1(move(t), [&] (U &&u) {
-        proc2(move(u), pass);
-      });
+  return [&](Epct<Proc<T, V>> pass) {
+    pass([&](T &&t, Epct<V> pass) {
+      proc1(move(t), [&](U &&u) { proc2(move(u), pass); });
     });
   };
 }
 
-template <typename P1, typename... Ps> constexpr auto pipe(P1 &&proc1,
-  Ps &&...procs) {
+template <typename P1, typename... Ps>
+constexpr auto pipe(P1 &&proc1, Ps &&... procs) {
   using T = typename RevProc<P1>::ArgType;
   using U = typename RevProc<P1>::RetType;
   using V = typename RevProc<typename Last<Ps...>::Type>::RetType;
-  return [&] (Epct<Proc<T, V>> &&pass) {
-    pipe(move(procs)...)([&] (Proc<U, V> &&proc2) {
+  return [&](Epct<Proc<T, V>> &&pass) {
+    pipe(move(procs)...)([&](Proc<U, V> &&proc2) {
       pipe(move(proc1), move(proc2))(move(pass));
     });
   };
@@ -146,30 +142,28 @@ template <typename P1, typename... Ps> constexpr auto pipe(P1 &&proc1,
 
 template <typename T, typename C> constexpr auto feed(T &&fed, C pass) {
   using U = typename RevEpct<C>::Type;
-  return [&] (Proc<T, U> &&proc) {
-    proc(move(fed), pass);
-  };
+  return [&](Proc<T, U> &&proc) { proc(move(fed), pass); };
 }
 
 // not used, but may be useful
-template <typename T> struct RevPartGen : public RevPartGen<decltype(&T::operator())> {};
-template <typename... Ts, typename C> struct RevPartGen<void (C::*)(Ts...) const> {
+template <typename T>
+struct RevPartGen : public RevPartGen<decltype(&T::operator())> {};
+template <typename... Ts, typename C>
+struct RevPartGen<void (C::*)(Ts...) const> {
   template <template <typename...> typename U> using Type = U<Ts...>;
 };
 
-
 int main() {
-  pipe([] (int &&x, Epct<NProc<int, int, int>> pass) {
-    pass(stand([&] (Epct<int> pass, Epct<PartGen<int, int>> intern_pass) {
-      intern_pass([&] (int &&y, int &&z) {
-        pass(y + z - x);
-      });
-    }));
-  }, [] (NProc<int, int, int> &&add_y_z_then_sub_x, Epct<Proc<int, int>> pass) {
-    add_y_z_then_sub_x(20, pass);
-  }, [] (Proc<int, int> &&add_20_z_then_sub_x, Epct<int> pass) {
-    add_20_z_then_sub_x(30, pass);
-  })(feed(42, [] (int &&eight) {
-    cout << eight << endl;
-  }));
+  pipe(
+      [](int &&x, Epct<NProc<int, int, int>> pass) {
+        pass(stand([&](Epct<int> pass, Epct<PartGen<int, int>> intern_pass) {
+          intern_pass([&](int &&y, int &&z) { pass(y + z - x); });
+        }));
+      },
+      [](NProc<int, int, int> &&add_y_z_then_sub_x, Epct<Proc<int, int>> pass) {
+        add_y_z_then_sub_x(20, pass);
+      },
+      [](Proc<int, int> &&add_20_z_then_sub_x, Epct<int> pass) {
+        add_20_z_then_sub_x(30, pass);
+      })(feed(42, [](int &&eight) { cout << eight << endl; }));
 }
--- proof/standarize7.cpp
+++ proof/standarize7.cpp
@@ -1,20 +1,17 @@
+#include <functional>
 #include <iostream>
 #include <string>
 #include <utility>
-#include <functional>
 
 using namespace std;
 
-
 template <typename T> using Func = std::function<T>;
 using Never = void;
-template <typename T> using Cont = Func<Never (T &&)>;
-template <typename T, typename U> using Proc = Func<Never (T &&, Cont<U>)>;
+template <typename T> using Cont = Func<Never(T &&)>;
+template <typename T, typename U> using Proc = Func<Never(T &&, Cont<U>)>;
 
 template <typename... Ts> struct _Last;
-template <typename T> struct _Last<T> {
-  using Type = T;
-};
+template <typename T> struct _Last<T> { using Type = T; };
 template <typename T, typename... Us> struct _Last<T, Us...> {
   using Type = typename _Last<Us...>::Type;
 };
@@ -25,56 +22,70 @@ template <typename T, typename U> struct Init<T, U> {
   template <template <typename...> class V> using Type = V<T>;
 };
 template <typename T, typename... Us> struct Init<T, Us...> {
-  template <template <typename...> class W> using T1 = typename Init<Us...>::template Type<W>; // T1: W<...>
+  template <template <typename...> class W>
+  using T1 = typename Init<Us...>::template Type<W>; // T1: W<...>
   template <typename... Ts> struct Foo {
     template <template <typename...> class W> using T2 = W<T, Ts...>;
   };
-  template <template <typename...> class V> using Type = typename T1<Foo>::template T2<V>;
+  template <template <typename...> class V>
+  using Type = typename T1<Foo>::template T2<V>;
 };
 
 // Proc<T1, Proc<T2, ... , Proc<Tn, U>...>>  <~>  GenProc<T1, T2, ... , Tn, U>
-//                                           ===  NestedProc<T1, T2, ... , Tn, U>
+//                                           ===  NestedProc<T1, T2, ... , Tn,
+//                                           U>
 template <typename... Ts> struct _GenProc {
   template <typename... Us> struct Foo {
     using RetType = Last<Ts...>;
-    using Type = Func<Never (Us &&..., Cont<RetType>)>;
+    using Type = Func<Never(Us &&..., Cont<RetType>)>;
   };
   using Type = typename Init<Ts...>::template Type<Foo>::Type;
 };
 template <typename... Ts> using GenProc = typename _GenProc<Ts...>::Type;
 
-template <typename T> struct ContTrait : public ContTrait<decltype(&T::operator())> {};
+template <typename T>
+struct ContTrait : public ContTrait<decltype(&T::operator())> {};
 template <typename T, typename C> struct ContTrait<Never (C::*)(T &&) const> {
   using Type = T;
 };
 
-template <typename T> struct ProcTrait : public ProcTrait<decltype(&T::operator())> {};
-template <typename T, typename U, typename C> struct ProcTrait<Never (C::*)(T &&, Cont<U>) const> {
+template <typename T>
+struct ProcTrait : public ProcTrait<decltype(&T::operator())> {};
+template <typename T, typename U, typename C>
+struct ProcTrait<Never (C::*)(T &&, Cont<U>) const> {
   using ArgType = T;
   using RetType = U;
 };
 
-template <typename T> struct GenProcTrait : public GenProcTrait<decltype(&T::operator())> {};
-template <typename T, typename U, typename C> struct GenProcTrait<Never (C::*)(T &&, Cont<U>) const> {
+template <typename T>
+struct GenProcTrait : public GenProcTrait<decltype(&T::operator())> {};
+template <typename T, typename U, typename C>
+struct GenProcTrait<Never (C::*)(T &&, Cont<U>) const> {
   template <template <typename...> class V> using ArgType = V<T>;
   using RetType = U;
   using NestedType = Proc<T, U>;
 };
-template <typename T, typename... Us, typename C> struct GenProcTrait<Never (C::*)(T &&, Us...) const> {
+template <typename T, typename... Us, typename C>
+struct GenProcTrait<Never (C::*)(T &&, Us...) const> {
   template <typename... Ts> struct Bar;
   template <typename... Ts> struct Bar<Ts &&...> {
-    template <template <typename...> class V> using Type = V<Ts..., typename ContTrait<Last<Us...>>::Type>;
+    template <template <typename...> class V>
+    using Type = V<Ts..., typename ContTrait<Last<Us...>>::Type>;
   };
-  using TailProc = typename Init<Us...>::template Type<Bar>::template Type<GenProc>;
+  using TailProc =
+      typename Init<Us...>::template Type<Bar>::template Type<GenProc>;
   template <typename... Ts> struct Foo {
     template <template <typename...> class V> using Type = V<T, Ts...>;
   };
-  template <template <typename...> class V> using ArgType = typename GenProcTrait<TailProc>::template ArgType<Foo>::template Type<V>;
+  template <template <typename...> class V>
+  using ArgType =
+      typename GenProcTrait<TailProc>::template ArgType<Foo>::template Type<V>;
   using RetType = typename GenProcTrait<TailProc>::RetType;
   using NestedType = Proc<T, typename GenProcTrait<TailProc>::NestedType>;
 };
 template <typename GP> using NestedProc = typename GenProcTrait<GP>::NestedType;
-template <typename... Ts> using Nested = typename GenProcTrait<GenProc<Ts...>>::NestedType;
+template <typename... Ts>
+using Nested = typename GenProcTrait<GenProc<Ts...>>::NestedType;
 
 template <typename... Ts> struct StandFoo {};
 template <typename T> struct StandFoo<T> {
@@ -87,30 +98,29 @@ template <typename T, typename... Us> struct StandFoo<T, Us...> {
   using Type = T;
   template <typename R> using BarType = GenProc<Us..., R>;
 };
-template <typename T, typename... Us> const bool StandFoo<T, Us...>::flag = false;
-template <typename GP> using SingleArg = typename GenProcTrait<GP>::template ArgType<StandFoo>;
+template <typename T, typename... Us>
+const bool StandFoo<T, Us...>::flag = false;
+template <typename GP>
+using SingleArg = typename GenProcTrait<GP>::template ArgType<StandFoo>;
 
-template <typename GP> auto stand_impl(GP &&gp, Cont<NestedProc<GP>> pass)
-  -> enable_if_t<SingleArg<GP>::flag> {
+template <typename GP>
+auto stand_impl(GP &&gp, Cont<NestedProc<GP>> pass)
+    -> enable_if_t<SingleArg<GP>::flag> {
   using R = typename GenProcTrait<GP>::RetType;
   using T = typename GenProcTrait<GP>::template ArgType<StandFoo>::Type;
-  pass([&] (T &&t, Cont<R> pass) {
-    gp(move(t), pass);
-  });
+  pass([&](T &&t, Cont<R> pass) { gp(move(t), pass); });
 }
 
 template <typename... Ts> struct StandBar {
   template <typename U, typename V, typename R, typename W>
   auto bar(U &&u, V &&gp, Cont<W> pass) {
-    pass([&] (Ts &&...ts, Cont<R> pass) {
-      gp(move(u), move(ts)..., pass);
-    });
+    pass([&](Ts &&... ts, Cont<R> pass) { gp(move(u), move(ts)..., pass); });
   }
 };
 
-
-template <typename GP> auto stand_impl(GP &&gp, Cont<NestedProc<GP>> pass)
-  -> enable_if_t<!SingleArg<GP>::flag> {
+template <typename GP>
+auto stand_impl(GP &&gp, Cont<NestedProc<GP>> pass)
+    -> enable_if_t<!SingleArg<GP>::flag> {
   // GP :: GenProc<int, int, int>
   // U  :: GenProc<int, int>
   // V  :: Proc<int, int>
@@ -118,11 +128,12 @@ template <typename GP> auto stand_impl(GP &&gp, Cont<NestedProc<GP>> pass)
   using T = typename GenProcTrait<GP>::template ArgType<StandFoo>::Type;
   using U = typename GenProcTrait<GP>::TailProc;
   using V = NestedProc<U>;
-  using W = typename GenProcTrait<GP>::template ArgType<StandFoo>::template BarType<R>;
-  pass([&] (T &&t, Cont<V> pass) {
+  using W = typename GenProcTrait<GP>::template ArgType<
+      StandFoo>::template BarType<R>;
+  pass([&](T &&t, Cont<V> pass) {
     using X = typename GenProcTrait<U>::template ArgType<StandBar>;
     X stdbar;
-    stdbar.template bar<T, GP, R, W>(move(t), move(gp), [&] (W &&tail_gen_proc) {
+    stdbar.template bar<T, GP, R, W>(move(t), move(gp), [&](W &&tail_gen_proc) {
       // pass :: Cont<Proc<int, int>>
       // tail_gen_proc :: GenProc<int, int>
       stand_impl(move(tail_gen_proc), pass);
@@ -135,13 +146,10 @@ template <typename GP> auto stand(GP &&gp, Cont<NestedProc<GP>> pass) {
 }
 
 int main() {
-  stand([] (int &&x, int &&y, Cont<int> pass) {
-    pass(x + y);
-  }, [] (auto &&proc) {
-    proc(3, [] (auto &&proc) {
-      proc(5, [] (int &&eight) {
-        cout << eight << endl;
-      });
-    });
-  });
+  stand([](int &&x, int &&y, Cont<int> pass) { pass(x + y); },
+        [](auto &&proc) {
+          proc(3, [](auto &&proc) {
+            proc(5, [](int &&eight) { cout << eight << endl; });
+          });
+        });
 }
